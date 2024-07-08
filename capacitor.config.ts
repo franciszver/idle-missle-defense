c937aed9546ea83af72610f77b1e7ad31561a0b7
franciszver <franciszver@outlook.com>
2024-07-07 21:32:49 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'idle-missle-defense',
+  webDir: 'www'
+};
+
+export default config;
